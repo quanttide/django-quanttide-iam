549227a204e94c3489c166ae502ca8e5d5f143c5
Guo Zhang <zhangguo@quanttide.com>
2021-06-28 17:14:34 +0800

    feature: 根据QtApps系统文档的Auth部分规定，修改is_anonymous字段的assert逻辑

--- django_qtauth/models.py
+++ django_qtauth/models.py
@@ -14,12 +14,12 @@ class AuthUser(models.Model):
     email = models.EmailField(unique=True, blank=True, null=True)
 
     # -- 用户权限标记 --
+    # 是否匿名
+    is_anonymous = models.BooleanField(default=True)
     # 是否激活
     is_active = models.BooleanField(default=False)
     # 是否认证
     is_authenticated = models.BooleanField(default=False)
-    # 是否匿名
-    is_anonymous = models.BooleanField(default=True)
     # 是否员工
     is_staff = models.BooleanField(default=False)
 
@@ -28,13 +28,12 @@ class AuthUser(models.Model):
     REQUIRED_FIELDS = ['user_id', 'is_authenticated']
 
     # --- assert语句 ---
-    if is_authenticated:
-        assert is_active, "未激活账号不可以通过认证"
     if is_anonymous:
-        # TODO：这条存疑
-        assert is_active and is_authenticated, "匿名用户必须是激活的认证用户"
+        assert not is_active and not is_authenticated and not is_staff, "匿名用户等同于未认证用户"
+    if is_authenticated:
+        assert not is_anonymous and is_active, "未激活账号不可以通过认证"
     if is_staff:
-        assert is_active and is_authenticated, "员工账号必须是激活的认证用户"
+        assert not is_anonymous and is_active and is_authenticated, "员工账号必须是激活的认证用户"
 
     # --- 设置 ---
     class Meta:
